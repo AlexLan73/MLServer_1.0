c7277781097a35ab2b299ff075d26e28c6258345
alexandr.lanin <alexlannami@gmail.com>
2020-07-09 14:35:56 +0300

    Start class Config

---
+++ Config/ConfigDan.py
@@ -0,0 +1,5 @@
+from Disk.ReadWrite import *
+
+class ConfigDan:
+    def __init__(self, re:ReadWrite):
+        print("  --- class ConfigDan --")
---
+++ Config/main_congif.py
@@ -0,0 +1,7 @@
+
+from Disk.ReadWrite import *
+from Config import *
+if __name__ == "__main__":
+    print(" ~~~  Test ")
+    _path = r"E:\MLserver\MLServer1.0"
+    _rw = ReadWrite(path_start =_path)
